fbfab50ee64419db9606c48fe1333391eeacdd38
jjingsse <42990623+jjingsse@users.noreply.github.com>
2019-01-10 15:22:03 +0900

    Add files via upload

---
+++ pro2/src/com/bit/pro2/model/BoardDto.java
@@ -0,0 +1,53 @@
+package pro2.model;
+
+public class BoardDto {
+	private int qnanum,qnapw,qnaphone;
+	private String qnaname,qnatitle,qnacont;
+	private String qnaresp;
+	public int getQnanum() {
+		return qnanum;
+	}
+	public void setQnanum(int qnanum) {
+		this.qnanum = qnanum;
+	}
+	public int getQnapw() {
+		return qnapw;
+	}
+	public void setQnapw(int qnapw) {
+		this.qnapw = qnapw;
+	}
+	public int getQnaphone() {
+		return qnaphone;
+	}
+	public void setQnaphone(int qnaphone) {
+		this.qnaphone = qnaphone;
+	}
+	public String getQnaname() {
+		return qnaname;
+	}
+	public void setQnaname(String qnaname) {
+		this.qnaname = qnaname;
+	}
+	public String getQnatitle() {
+		return qnatitle;
+	}
+	public void setQnatitle(String qnatitle) {
+		this.qnatitle = qnatitle;
+	}
+	public String getQnacont() {
+		return qnacont;
+	}
+	public void setQnacont(String qnacont) {
+		this.qnacont = qnacont;
+	}
+	public String getQnaresp() {
+		return qnaresp;
+	}
+	public void setQnaresp(String qnaresp) {
+		this.qnaresp = qnaresp;
+	}
+
+	
+
+
+}
